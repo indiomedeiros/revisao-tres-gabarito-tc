179fd5af9e06049fee9c11534382112f72bf2547
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2024-04-04 09:13:14 -0300

    Resolve questão 5

---
+++ loops.py
@@ -0,0 +1,12 @@
+def contar_ate_vinte():
+#Exemplo com for
+#   for numero in range(21):
+#     print(numero)
+
+#Exemplo com while
+  contador = 0
+  while(contador < 21):
+      print(contador)
+      contador = contador + 1
+
+contar_ate_vinte()
