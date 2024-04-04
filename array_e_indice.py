bdec1ff058d2186b34121b5274c5a1cbb197e28b
Índio Medeiros <71137294+indiomedeiros@users.noreply.github.com>
2024-04-04 08:59:28 -0300

    Resolve a questão 1

---
+++ array_e_indice.py
@@ -0,0 +1,10 @@
+def imprimir_nomes():
+  nomes = ["João", "Maria", "Fulano", "Ciclano"]
+  print("1 -", nomes[0])
+  print("2 -", nomes[1])
+  print("3 -", nomes[2])
+  print("4 -", nomes[3])
+
+imprimir_nomes()
+
+
